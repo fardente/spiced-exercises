f55935fa3421062e6d58ee0e0334493256d198f1
Fernando Ardente <ardente@mailbox.org>
2021-05-11 17:10:32 +0200

    Added Control Flow - logType exercise

---
+++ 20210511-ControlFlow/logType.js
@@ -0,0 +1,40 @@
+function logType(arg) {
+    console.log("Input: ", arg, "Type: ", typeof arg);
+    if (typeof arg === "undefined") {
+        console.log("undefined!");
+    } else if (arg === null) {
+        console.log("null!");
+    } else if (typeof arg === "number" && !isNaN(arg)) {
+        console.log("number!");
+    } else if (Number.isNaN(arg)) {
+        console.log("not a number!");
+    } else if (typeof arg === "string") {
+        console.log("string!");
+    } else if (typeof arg === "boolean") {
+        console.log("boolean!");
+    } else if (typeof arg === "bigint") {
+        console.log("bigint!");
+    } else if (typeof arg === "function") {
+        console.log("funtion!");
+    } else if (typeof arg === "object") {
+        console.log("object!");
+    } else if (Array.isArray(arg)) {
+        console.log("array!");
+    } else {
+        console.log("I have no idea!");
+    }
+}
+
+var t;
+logType(t);
+logType(null);
+logType(3);
+logType(2 * "asd");
+logType("word");
+logType(false);
+logType(5n);
+logType(console.log);
+logType({});
+logType({ sample: "object" });
+logType([1, 2, 3, 4]);
+logType(new Array(1, 2, 3));
