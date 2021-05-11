3fbd539e6d4dc07b9cb62935ffbeb417d7747db1
Fernando Ardente <ardente@mailbox.org>
2021-05-11 17:44:05 +0200

    Added Control Flow excercise 3

---
+++ 20210511-ControlFlow/ex3.js
@@ -0,0 +1,11 @@
+// Write a while or for loop that counts down from 10 to 1, logging each number to the console.
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
+
+let i = 10;
+while (i > 0) {
+    console.log(i);
+    i--;
+}
