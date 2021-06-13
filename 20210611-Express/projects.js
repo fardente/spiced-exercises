23350ba15516e22cc1a9961a217d575a7547e018
Fernando Ardente <ardente@mailbox.org>
2021-06-13 21:54:45 +0200

    Added Express Cookies exercise

---
+++ 20210611-Express/projects.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+function listDirHTML(dir) {
+    const folderContents = fs.readdirSync(dir, { withFileTypes: true });
+    let output = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Projects</title>
+    </head>
+    <body>
+    <ul>
+    `;
+    folderContents.forEach((element, index) => {
+        output += makeHTML(element);
+    });
+    return output + "</ul>\n</body></html>";
+}
+
+function makeHTML(element) {
+    const html = `<li><a target="_blank" href=${element.name}>${element.name}</a></li>\n`;
+    return html;
+}
+
+module.exports = listDirHTML;
