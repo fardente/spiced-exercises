dc3f199d665e8201bf01b67b00666979115a4bc4
Fernando Ardente <ardente@mailbox.org>
2021-06-16 18:24:40 +0200

    Tweets get retrieved and parsed

--- 20210616-TwitterAPI/index.js
+++ 20210616-TwitterAPI/index.js
@@ -6,7 +6,7 @@ const HOST = "https://api.twitter.com";
 const PATH = "/oauth2/token";
 
 function encodeCredentials() {
-    console.log("KEY", Key, "SECRET", Secret);
+    // console.log("KEY", Key, "SECRET", Secret);
     return Buffer.from(`${Key}:${Secret}`).toString("base64");
 }
 
@@ -31,7 +31,8 @@ function makeRequest(
                 responseBody += chunk;
             });
             response.on("end", function () {
-                console.log("Response end", responseBody);
+                // console.log("Response end", responseBody);
+                responseBody = JSON.parse(responseBody);
                 callback(responseBody);
             });
             response.on("error", function (error) {
@@ -40,11 +41,12 @@ function makeRequest(
         }
     );
     // console.log("Request:", request);
+    // console.log(requestHeaders);
     request.end(requestBody);
 }
 
-function getToken() {
-    console.log("encoded creds", encodeCredentials());
+function getToken(callback) {
+    // console.log("encoded creds", encodeCredentials());
     const headers = {
         Authorization: `Basic ${encodeCredentials()}`,
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
@@ -56,14 +58,54 @@ function getToken() {
         "/oauth2/token",
         headers,
         body,
-        function () {
-            console.log("Callback gettoken called");
+        (responseBody) => {
+            const token = responseBody["access_token"];
+            callback(token);
         }
     );
 }
 
-getToken();
+function getTweets(token, callback) {
+    // console.log("responsebody", responseBody, responseBody["access_token"]);
+    const screenName = "QuantaMagazine",
+        count = 3;
+    const host = "api.twitter.com";
+    const path = `/1.1/statuses/user_timeline.json?tweet_mode=extended&exclude_replies=true&trim_user=true&screen_name=${screenName}&count=${count}`;
+    const query = "asd";
+    const headers = {
+        Authorization: "Bearer " + token,
+    };
+    makeRequest("GET", host, path, headers, null, (tweets) => {
+        // console.log("gettweets ", tweets);
+        callback(tweets);
+    });
+}
 
-// function getTweets(){
-//     makeRequest()
-// }
+function parseTweets(tweets, callback) {
+    tweets = tweets.map((tweet) => {
+        const text = tweet.full_text.split("https:")[0];
+        // console.log(text);
+        const url = tweet.entities.urls[0].url;
+        // console.log(url);
+        return {
+            text,
+            url,
+        };
+    });
+    callback(tweets);
+}
+
+function getTickerItems(callback) {
+    getToken((token) => {
+        getTweets(token, (tweets) => {
+            parseTweets(tweets, (parsedTweets) => {
+                // console.log("Parsed Tweeties", parsedTweets);
+                callback(parsedTweets);
+            });
+        });
+    });
+}
+
+getTickerItems((tweets) => {
+    console.log(tweets);
+});
