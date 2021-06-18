718ed12fcb6e5b27bdbc2c8b124735af458f0522
Fernando Ardente <ardente@mailbox.org>
2021-06-18 12:50:46 +0200

    Twitter API - cleaned code

--- 20210617-TwitterAPIPromises/index.js
+++ 20210617-TwitterAPIPromises/index.js
@@ -6,7 +6,6 @@ const HOST = "https://api.twitter.com";
 const PATH = "/oauth2/token";
 
 function encodeCredentials() {
-    // console.log("KEY", Key, "SECRET", Secret);
     return Buffer.from(`${Key}:${Secret}`).toString("base64");
 }
 
@@ -25,7 +24,6 @@ function makeRequest(method, host, path, requestHeaders, requestBody) {
                     responseBody += chunk;
                 });
                 response.on("end", function () {
-                    // console.log("Response end", responseBody);
                     try {
                         responseBody = JSON.parse(responseBody);
                     } catch (error) {
@@ -39,14 +37,11 @@ function makeRequest(method, host, path, requestHeaders, requestBody) {
                 });
             }
         );
-        // console.log("Request:", request);
-        // console.log(requestHeaders);
         request.end(requestBody);
     });
 }
 
 function getToken() {
-    console.log("Token was requested");
     const headers = {
         Authorization: `Basic ${encodeCredentials()}`,
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
@@ -72,34 +67,12 @@ function getTweets(screenName, count, token) {
     return makeRequest("GET", host, path, headers);
 }
 
-// function getTweets(screenName, count, token) {
-//     return new Promise((resolve, reject) => {
-//         // console.log("responsebody", responseBody, responseBody["access_token"]);
-//         // const screenName = "QuantaMagazine",
-//         const host = "api.twitter.com";
-//         const path = `/1.1/statuses/user_timeline.json?tweet_mode=extended&exclude_replies=true&trim_user=true&screen_name=${screenName}&count=${count}`;
-//         const headers = {
-//             Authorization: "Bearer " + token,
-//         };
-//         makeRequest("GET", host, path, headers)
-//             .then((tweets) => {
-//                 // console.log("gettweets ", tweets);
-//                 resolve(tweets);
-//             })
-//             .catch((error) => {
-//                 reject(error);
-//             });
-//     });
-// }
-
 function parseTweets(tweets, screenName) {
     tweets = tweets
         .filter((tweet) => tweet.entities.urls.length)
         .map((tweet) => {
             const text = tweet.full_text.split("http")[0].trim();
-            // console.log(tweet);
             const url = tweet.entities.urls[0].url;
-            // console.log(url);
             return {
                 text: `${text} (${screenName})`,
                 url,
@@ -111,74 +84,7 @@ function parseTweets(tweets, screenName) {
 function getTickerItems(screenName, count, token) {
     return getTweets(screenName, count, token).then((tweets) => {
         return parseTweets(tweets, screenName);
-        // return parseTweets(tweets, screenName);
     });
-    // .then((parsedTweets) => {
-    //     console.log("parsed Tweets", parsedTweets);
-    //     return parsedTweets;
-    // });
 }
 
-// function getTickerItems(screenName, count) {
-//     return getToken()
-//         .then((token) => {
-//             return getTweets(screenName, count, token);
-//         })
-//         .then((tweets) => {
-//             return parseTweets(tweets, screenName);
-//         });
-//     // .then((parsedTweets) => {
-//     //     console.log("parsed Tweets", parsedTweets);
-//     //     return parsedTweets;
-//     // });
-// }
-
-// console.log(getTickerItems("zeitonline", 3));
-
-// getToken().then((token) => {
-//     getTickerItems("zeitonline", 2, token);
-// });
-
-// getTickerItems("zeitonline", 3).then((tweets) => {
-//     console.log("gettickeritems", tweets);
-// });
-
-// function getTickerItems(screenName, count) {
-//     return new Promise((resolve, reject) => {
-//         getToken().then((token) => {
-//             getTweets(screenName, count, token).then((tweets) => {
-//                 parseTweets(tweets).then((parsedTweets) => {
-//                     resolve(parsedTweets);
-//                 });
-//             });
-//         });
-//     });
-// }
-
-// getTickerItems("heiseonline", 3).then((res) => {
-//     console.log(res);
-// });
-
 module.exports = { getTickerItems, getToken };
-
-// function promiseTest(input) {
-//     return new Promise((resolve, reject) => {
-//         resolve("input " + input);
-//     });
-// }
-
-// console.log(
-//     promiseTest("eins")
-//         .then((res1) => {
-//             console.log("then1 ", res1);
-//             return "then1 return";
-//         })
-//         .then((res2) => {
-//             console.log("then 2 ", res2);
-//             return promiseTest("middle");
-//         })
-//         .then((res3) => {
-//             console.log("then3 ", res3);
-//             promiseTest("neues call");
-//         })
-// );
